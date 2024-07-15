de7f91efe0d631f9b176424da9af082427fc3ace
Merve Nur Güler <72803135+MerveNurGuler@users.noreply.github.com>
2024-07-15 19:19:33 +0300

    Create second-degree_simulation.sh

---
+++ second-degree_simulation.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+PEDSIM_PATH=/path/to/ped-sim
+VCF_PATH=/path/to/founders_200k_final.vcf
+MAP_PATH=/path/to/inter_biSNPs_lbk_200K.map
+SEX_PATH=/path/to/founders_200K.fam
+INTF_PATH=$PEDSIM_PATH/interfere/nu_p_campbell_X.tsv
+
+first=1
+last=60
+
+for ((i=$first; i <= $last; i++))
+do
+  echo "RUN $i of $last"
+  mkdir run_$i
+  for ((j=1; j <= 4; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/grandparent-grandchild_$j.def -m $MAP_PATH -o ./run_$i/grandparent-grandchild_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 4; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/grandparent-grandchild_sibs_inb_$j.def -m $MAP_PATH -o ./run_$i/grandparent-grandchild_sibs_inb_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 4; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/grandparent-grandchild_1st-cous_inb_$j.def -m $MAP_PATH -o ./run_$i/grandparent-grandchild_1st-cous_inb_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 16; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/grandparent-grandchild_2nd-cous_inb_$j.def -m $MAP_PATH -o ./run_$i/grandparent-grandchild_2nd-cous_inb_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 8; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/avuncular_$j.def -m $MAP_PATH -o ./run_$i/avuncular_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 32; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/avuncular_1st-cous_inb_$j.def -m $MAP_PATH -o ./run_$i/avuncular_1st-cous_inb_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 128; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/avuncular_2nd-cous_inb_$j.def -m $MAP_PATH -o ./run_$i/avuncular_2nd-cous_inb_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 6; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/half-siblings_$j.def -m $MAP_PATH -o ./run_$i/half-siblings_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+  for ((j=1; j <= 4; j++))
+  do
+    $PEDSIM_PATH/ped-sim -d /path/to/def/half-siblings_sibs_inb_$j.def -m $MAP_PATH -o ./run_$i/half-siblings_sibs_inb_$j -i $VCF_PATH -X X --intf $INTF_PATH --sexes $SEX_PATH --keep_phase --founder_ids --fam --miss_rate 0
+  done
+done
